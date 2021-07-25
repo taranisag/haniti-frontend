e0cc028cb8c1d5084854465efe05ac05a9c0ba70
Ori Harel <ori.harel@gmail.com>
2021-07-25 18:51:03 +0300

    notif

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { ReactSVG } from "react-svg";
-import { login } from "./services/login";
+import firebase from "firebase";
 import logo from "./parking_test.svg";
 import "./App.css";
 import { app } from "./services/api";
@@ -12,6 +12,19 @@ const sucess = (result: any) => {
 const error = (result: any) => {
   console.error(result);
 };
+
+const messaging = firebase.messaging();
+
+messaging.getToken({ vapidKey: process.env.REACT_APP_PUSH_KEY_PAIR }).then((currentToken) => {
+    if (currentToken) {
+        app.put("/push-token", {pushToken: currentToken}).then((result) => console.log(result));
+    } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+    }
+}).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+});
 
 function App() {
   useEffect(() => {
