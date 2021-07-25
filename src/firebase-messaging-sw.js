86e129f3e305a077272142d140e98f77ee2f5f17
Ori Harel <ori.harel@gmail.com>
2021-07-25 20:41:56 +0300

    fix sw

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,30 @@
+import firebase from "firebase";
+
+importScripts('https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js');
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+    self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
